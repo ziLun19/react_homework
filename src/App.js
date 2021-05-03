6eaea8ebabaf12b668206634cd2fb489d2587a7c
ziLun19 <b0829049@cgu.edu.tw>
2021-05-03 14:23:54 +0800

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import hw3 from './hw3';
+
+function App() {
+  return (
+    <div className="App">
+     {hw3()}
+    </div>
+  );
+}
+
+export default App;
